2817c397298e12076684150b1a4b35472f2dd3ca
Alpha1916 <alpha1916@gmail.com>
2018-02-08 12:04:01 +0800

    modified at Introduce.vue and delete unused code

--- client/src/components/Introduce.vue
+++ client/src/components/Introduce.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
     <h1>介绍</h1>
-    
+
   </div>
 </template>
 
 <script>
-export default {
-  name: "Introduce"
-};
+  export default {
+    name: "Introduce"
+  };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  font-weight: normal;
-}
+  h1 {
+    font-weight: normal;
+  }
+
 </style>
+
